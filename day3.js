45ac1bc8169511f5224a10036b482ebb7e758b9c
ljubo92 <ljubo12@hotmail.com>
2020-12-06 23:13:27 +0100

    testish

--- day3.js
+++ day3.js
@@ -1,6 +1,5 @@
 const data = require("fs").readFileSync("input.txt", "utf8");
 
-var counter = 0
-const input = data.split(/\r\n\r\n/g).map(x=> x.split(/\r\n/g)).map(x=> [...x[0]].map(l=> x.every(e=> e.includes(l)) && counter++))
-
-console.log(counter)
+var i = 0
+data.split(/\r\n\r\n/g).map(x=> x.split(/\r\n/g)).map(x=> [...x[0]].map(l=> x.every(e=> e.includes(l)) && i++))
+console.log(i)
