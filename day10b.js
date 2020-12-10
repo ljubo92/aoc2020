fd35b8d20dc7bc4bcfd6ad3cec5480fbe6713c77
Ljubo <36992530+ljubo92@users.noreply.github.com>
2020-12-11 00:47:16 +0100

    Create day10b.js

---
+++ day10b.js
@@ -0,0 +1,18 @@
+const data = require("fs").readFileSync("input.txt", "utf8");
+const parsedInput = data.split(/\r\n/g).map(x => parseInt(x.split(" "))).sort((x, y) => x - y)
+
+var storage = {};
+
+const damschkie = (arr, storage) => {
+    const key = arr.toString();
+    if (key in storage) return storage[key]
+    let sum = 1;
+    for(var i = 0 ; i < arr.length; i++){
+        if (arr[i+2]-arr[i] <= 3) {
+            sum += rec([arr[i],...arr.slice(i+2)], storage)
+        }
+    }
+    storage[key] = sum;
+    return sum
+}
+let z = damschkie([0,...parsedInput],storage)
