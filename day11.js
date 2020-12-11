6ef8b587b9d94d3b7d04c37642a4d910b2e7156a
Ljubo <36992530+ljubo92@users.noreply.github.com>
2020-12-12 00:22:06 +0100

    Update day11.js

--- day11.js
+++ day11.js
@@ -1,26 +1,26 @@
 const data = require("fs").readFileSync("input.txt", "utf8");
 const seatsMatrix = data.split(/\r\n/g).map(x => x.split(""));
 
-let countSeats = (matrix) => matrix.reduce((cr,ar) => ar.reduce((c,a) => a === '#' ? c+1 : c, cr) ,0)
-let compareTwoMatrixes = (a, b) =>  JSON.stringify(a) === JSON.stringify(b)
+let countSeats = (matrix) => matrix.reduce((cr, ar) => ar.reduce((c, a) => a === '#' ? c + 1 : c, cr), 0)
+let compareTwoMatrixes = (a, b) => JSON.stringify(a) === JSON.stringify(b)
 
-let directions =  [[1,1],[1,0],[-1,-1],[0,-1],[1,-1],[-1,0],[0,1],[-1,1]]
+let directions = [[1, 1], [1, 0], [-1, -1], [0, -1], [1, -1], [-1, 0], [0, 1], [-1, 1]]
 
 const countNeighbors = (m, seatRow, seatCol) => {
-    let neighbors =0;
-    for(let k = 0; k < directions.length; k++){
-        let ii = seatRow;  
-        let jj = seatCol; 
+    let neighbors = 0;
+    for (let k = 0; k < directions.length; k++) {
+        let ii = seatRow;
+        let jj = seatCol;
         for (let i = 0; i < m.length; i++) {
-            ii +=  directions[k][0]
+            ii += directions[k][0]
             jj += directions[k][1];
             if (ii < 0 || ii >= m.length) continue
-                if (jj < 0 || jj >= m[0].length || (ii === seatRow && jj === seatCol)) continue
-                    if (m[ii][jj] === '#') {
-                        neighbors++;
-                        break;
-                }
-            if(m[ii][jj] === 'L') break
+            if (jj < 0 || jj >= m[0].length || (ii === seatRow && jj === seatCol)) continue
+            if (m[ii][jj] === '#') {
+                neighbors++;
+                break;
+            }
+            if (m[ii][jj] === 'L') break
         }
     }
     return neighbors
@@ -31,8 +31,8 @@ const swapSeats = (matrix) => {
     for (let k = 0; k < matrix.length; k++) {
         for (let g = 0; g < matrix[0].length; g++) {
             if (matrix[k][g] === 'L' && countNeighbors(matrix, k, g) === 0) newMatrix[k][g] = '#';
-                else if (matrix[k][g] === '#' && countNeighbors(matrix, k, g) >= 5)  newMatrix[k][g] = 'L';
-                    else newMatrix[k][g] = matrix[k][g]
+            else if (matrix[k][g] === '#' && countNeighbors(matrix, k, g) >= 5) newMatrix[k][g] = 'L';
+            else newMatrix[k][g] = matrix[k][g]
         }
     }
     if (compareTwoMatrixes(matrix, newMatrix)) return newMatrix
@@ -41,5 +41,3 @@ const swapSeats = (matrix) => {
 
 const z = swapSeats(seatsMatrix);
 console.log(countSeats(z))
-
-
