2e540f6adb168582ff8a0308ac2bf76fe58332b2
Ljubo <36992530+ljubo92@users.noreply.github.com>
2020-12-12 15:21:08 +0100

    Update day12.js
    tried rotating with rotationmatrix instead of calculations

--- day12.js
+++ day12.js
@@ -4,24 +4,16 @@ let parsedInput = data.split(/\r\n/g).map(x => ({ key: x.charAt(0), value: parse
 let shipX = 0, shipY = 0, pointX = 10, pointY = 1;
 
 rotate = (x) => {
+    if (x.key === 'L') x.value *= -1
+    var radians = (Math.PI / 180) * x.value;
     const tmp = pointX;
-    if (x.value === 180) {
-        pointX = -tmp
-        pointY = -pointY
-    }
-    let dir = 1;
-    if (x.key === 'L') dir = -1
-    if (x.value === 90) {
-        pointX = pointY * dir
-        pointY = -tmp * dir
-    } else if (x.value === 270) {
-        pointX = -pointY * dir
-        pointY = tmp * dir
-    }
+    pointX = Math.cos(radians) * pointX + Math.sin(radians) * pointY
+    pointY = Math.cos(radians) * pointY - Math.sin(radians) * tmp
 }
+
 stepBoat = (x) => {
-    shipX += (x * pointX)
-    shipY += (x * pointY)
+    shipX += x * pointX
+    shipY += x * pointY
 }
 
 parsedInput.map(x => ((x.key === 'F') ? stepBoat(x.value) :
@@ -32,5 +24,3 @@ parsedInput.map(x => ((x.key === 'F') ? stepBoat(x.value) :
                     (x.key === 'R' || x.key === 'L') ? rotate(x) : 0))
 
 console.log(Math.abs(shipX) + Math.abs(shipY))
-
-
