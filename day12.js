293ae77a242db2f9d00babdc02ea3877d37124ec
Ljubo <36992530+ljubo92@users.noreply.github.com>
2020-12-12 13:31:52 +0100

    Create day12.js

---
+++ day12.js
@@ -0,0 +1,42 @@
+const data = require("fs").readFileSync("input.txt", "utf8");
+let parsedInput = data.split(/\r\n/g).map(x => ({ key: x.charAt(0), value: parseInt(x.substring(1)) }))
+
+let shipX = 0, shipY = 0, pointX = 10, pointY = 1;
+
+rotate = (x) => {
+    const tmp = pointX;
+    if (x.value === 180) {
+        pointX = -tmp
+        pointY = -pointY
+    }
+    let dir = 1;
+    if (x.key === 'L') dir = -1
+    if (x.value === 90) {
+        pointX = pointY * dir
+        pointY = -tmp * dir
+    } else if (x.value === 270) {
+        pointX = -pointY * dir
+        pointY = tmp * dir
+    }
+}
+stepBoat = (x) => {
+    shipX += (x * pointX)
+    shipY += (x * pointY)
+}
+moveBoat = (x) => {
+    if (pointX >= 0 && pointY >= 0) stepBoat(x)
+    else if (pointX >= 0 && pointY <= 0) stepBoat(x) 
+    else if (pointX <= 0 && pointY >= 0) stepBoat(x)
+    else if (pointX <= 0 && pointY <= 0) stepBoat(x)
+}
+
+parsedInput.map(x => ((x.key === 'F') ? moveBoat(x.value) :
+    (x.key === 'E') ? pointX += x.value :
+        (x.key === 'S') ? pointY -= x.value :
+            (x.key === 'W') ? pointX -= x.value :
+                (x.key === 'N') ? pointY += x.value :
+                    (x.key === 'R' || x.key === 'L') ? rotate(x) : 0))
+
+console.log(Math.abs(shipX) + Math.abs(shipY))
+
+
